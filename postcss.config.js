df163ceda684adb66b6123ac9c1df1000c60e14e
Theenadayalan K <theenadayalan.k@zohocorp.com>
2019-09-07 14:16:49 +0530

    Added purge css to remove unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,24 @@
+const autoprefixer = require('autoprefixer');
+const postcssSass = require('@csstools/postcss-sass');
+const cssnano = require('cssnano');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const purgeCSS = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.js',
+    './public/index.html'
+  ],
+  css: ['./src/**/*.css', './src/**/*.scss']
+});
+
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    require('@csstools/postcss-sass'),
-    require('cssnano')({
+    autoprefixer,
+    postcssSass,
+    isProduction ? cssnano({
       preset: 'default'
-    })
+    }) : null,
+    isProduction ? purgeCSS : null
   ]
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,13 +4,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
-    open: true,
-    hot: true,
-    compress: true,
     historyApiFallback: true
   }
 });
