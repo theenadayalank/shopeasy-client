52569fc0fb111f8ed0e1607e032c8dd117bc95d6
Theenadayalan K <theenadayalan.k@zohocorp.com>
2019-09-07 16:18:06 +0530

    Added SPA conf for netlify

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,19 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: false,
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'public', '_redirects'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ])
   ]
 });
