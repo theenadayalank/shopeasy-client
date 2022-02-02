9cd77eb2f20e1aaa1d3c286d2de6b783577099f7
Theenadayalan Kaliamurthy <puduvai.theena@gmail.com>
2022-02-03 00:15:28 +0530

    Webpack 5 upgrade

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, 'dist'),
     port: 9000,
     historyApiFallback: true
   }
